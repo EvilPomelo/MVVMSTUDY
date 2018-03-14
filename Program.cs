25d59deac9e1452f0c1e7546bb2c94c155ccf4ae
EvilPomelo <123227682@qq.com>
2018-03-14 18:12:17 +0800

    init

---
+++ Program.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMStudy
+{
+    class Program
+    {
+        public string Name { get; set; }
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+            
+            }
+        }
+        private int myVar;
+    }
+}
